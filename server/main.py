ef5039afd4a1ca13f175e8ab70204a0c18ea9ec3
Asaf31214 <asaf.kanlipicak@outlook.com>
2025-01-18 02:11:17 +0300

    env try again
    Took 6 minutes

--- server/main.py
+++ server/main.py
@@ -1,17 +1,26 @@
+import os
+
 import uvicorn
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
+load_dotenv()
 app = FastAPI()
 
 
 @app.get("/health")
-def health_check():
+async def health_check():
     return {"status": "healthy"}
 
 
 @app.get("/")
-def root():
+async def root():
     return {"message": "Hello World"}
+
+@app.get("/env")
+async def env():
+    key = os.environ.get("API_KEY")
+    return {"key": key}
 
 
 if __name__ == "__main__":
