fcc1bdb902f7b1cad3099480b11a8acbebfecc31
Asaf31214 <asaf.kanlipicak@outlook.com>
2025-01-17 23:26:29 +0300

    root endpoint
    Took 4 minutes

--- server/main.py
+++ server/main.py
@@ -9,5 +9,10 @@ def health_check():
     return {"status": "healthy"}
 
 
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
